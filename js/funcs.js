12be85a52125b2c1ba24f11531c0e90226417098
guazipi <sam_lhx@126.com>
2016-04-23 15:50:53 +0800

    add another fancybox

---
+++ js/funcs.js
@@ -0,0 +1,74 @@
+/**
+ * Created by Administrator on 2016/4/23.
+ */
+$("a.various").fancybox({
+    autoDimensions: !1,
+    autoScale: !1,
+    scrolling: "no",
+    width:1080,
+    height:700,
+    speedIn:600,
+    speedOut:200,
+    overflow: "hidden",
+    hideOnOverlayClick: !0,
+});
+function showShirtInApp(styleName) {
+    $(".polo-real").html("");
+    var poloReal = document.getElementsByClassName("polo-real")[0];
+    switch (styleName) {
+        case "huanghei":
+            var divArr = getHtmlFromStyleName("huanghei/huanghei")
+            $.each(divArr, function (key, val) {
+                poloReal.appendChild(val);
+            })
+            break;
+        case "huanglan":
+            var divArr = getHtmlFromStyleName("huanglan/hl")
+            $.each(divArr, function (key, val) {
+                poloReal.appendChild(val);
+            })
+            break;
+        case "huise":
+            var divArr = getHtmlFromStyleName("huise/hs")
+            $.each(divArr, function (key, val) {
+                poloReal.appendChild(val);
+            })
+            break;
+        case "lanhei":
+            var divArr = getHtmlFromStyleName("lanhei/lanhei")
+            $.each(divArr, function (key, val) {
+                poloReal.appendChild(val);
+            })
+            break;
+    }
+}
+function getHtmlFromStyleName(styleName) {
+    var divArr = [];
+    var div1 = document.createElement("div");
+    div1.style.backgroundImage = "url(./images/wholePolo/" + styleName + 1 + ".jpg)";
+    div1.style.backgroundSize = "cover";
+    div1.style.display = "block";
+    div1.className = "piece";
+    divArr.push(div1);
+    var div2 = document.createElement("div");
+    div2.style.backgroundImage = "url(./images/wholePolo/" + styleName + 2 + ".jpg)";
+    div2.style.backgroundSize = "cover";
+    div2.style.display = "none";
+    div2.className = "piece";
+    divArr.push(div2);
+
+    var div3 = document.createElement("div");
+    div3.style.backgroundImage = "url(./images/wholePolo/" + styleName + 3 + ".jpg)";
+    div3.style.backgroundSize = "cover";
+    div3.style.display = "none";
+    div3.className = "piece";
+    divArr.push(div3);
+
+    var div4 = document.createElement("div");
+    div4.style.backgroundImage = "url(./images/wholePolo/" + styleName + 4 + ".jpg)";
+    div4.style.backgroundSize = "cover";
+    div4.style.display = "none";
+    div4.className = "piece";
+    divArr.push(div4);
+    return divArr;
+}
--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
  * Created by Administrator on 2016/4/22.
  */
 $(document).ready(function () {
-
-
     $(".rotate-left").bind("click", function () {
         $(".polo-real div").each(function (index) {
             if (this.style.display == "block") {
@@ -51,7 +49,6 @@ $(document).ready(function () {
 
     $(".collar-shape div").each(function () {
         this.onclick = function () {
-            console.log(this.querySelector("div img"));
             if (!this.querySelector("div img")) {
                 $(".collar-shape div").each(function () {
                     this.innerHTML = "";
@@ -62,76 +59,18 @@ $(document).ready(function () {
             }
         }
     });
-    $("a.various").fancybox({
-        autoDimensions: !1,
-        autoScale: !1,
-        scrolling: "no",
-        width:1080,
-        height:700,
-        speedIn:600,
-        speedOut:200,
-        overflow: "hidden",
-        hideOnOverlayClick: !0,
+    $(".collar-collor div").each(function () {
+        this.onclick = function () {
+            if (!this.querySelector("div img")) {
+                $(".collar-collor div").each(function () {
+                    this.innerHTML = "";
+                });
+                var img = document.createElement("img");
+                img.src = "./images/tick.png";
+                this.appendChild(img);
+            }
+        }
     });
+
+
 });
-
-function showShirtInApp(styleName) {
-    $(".polo-real").html("");
-    var poloReal = document.getElementsByClassName("polo-real")[0];
-    switch (styleName) {
-        case "huanghei":
-            var divArr = getHtmlFromStyleName("huanghei/huanghei")
-            $.each(divArr, function (key, val) {
-                poloReal.appendChild(val);
-            })
-            break;
-        case "huanglan":
-            var divArr = getHtmlFromStyleName("huanglan/hl")
-            $.each(divArr, function (key, val) {
-                poloReal.appendChild(val);
-            })
-            break;
-        case "huise":
-            var divArr = getHtmlFromStyleName("huise/hs")
-            $.each(divArr, function (key, val) {
-                poloReal.appendChild(val);
-            })
-            break;
-        case "lanhei":
-            var divArr = getHtmlFromStyleName("lanhei/lanhei")
-            $.each(divArr, function (key, val) {
-                poloReal.appendChild(val);
-            })
-            break;
-    }
-}
-function getHtmlFromStyleName(styleName) {
-    var divArr = [];
-    var div1 = document.createElement("div");
-    div1.style.backgroundImage = "url(./images/wholePolo/" + styleName + 1 + ".jpg)";
-    div1.style.backgroundSize = "cover";
-    div1.style.display = "block";
-    div1.className = "piece";
-    divArr.push(div1);
-    var div2 = document.createElement("div");
-    div2.style.backgroundImage = "url(./images/wholePolo/" + styleName + 2 + ".jpg)";
-    div2.style.backgroundSize = "cover";
-    div2.style.display = "none";
-    div2.className = "piece";
-    divArr.push(div2);
-
-    var div3 = document.createElement("div");
-    div3.style.backgroundImage = "url(./images/wholePolo/" + styleName + 3 + ".jpg)";
-    div3.style.backgroundSize = "cover";
-    div3.style.display = "none";
-    div3.className = "piece";
-    divArr.push(div3);
-
-    var div4 = document.createElement("div");
-    div4.style.backgroundImage = "url(./images/wholePolo/" + styleName + 4 + ".jpg)";
-    div4.style.backgroundSize = "cover";
-    div4.style.display = "none";
-    div4.className = "piece";
-    divArr.push(div4);
-    return divArr;
-}
