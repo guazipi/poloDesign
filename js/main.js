621b1d35bc6b3654aec316186555236225f784b5
guazipi <sam_lhx@126.com>
2016-04-23 19:44:02 +0800

    style adjust

--- js/main.js
+++ js/main.js
@@ -72,5 +72,126 @@ $(document).ready(function () {
         }
     });
 
+    $(".steps div").each(function (index) {
+        this.onclick = function () {
+            $(".steps div").each(function () {
+                this.style.borderRight = "1px solid rgb(215, 215, 215)";
+                this.style.borderTop = "1px solid rgb(215, 215, 215)";
+                this.style.borderLeft = "1px solid rgb(215, 215, 215)";
+                this.style.borderBottom = "1px solid rgb(215, 215, 215)";
+            })
+            var temp = index;
+            //steps中点击相应的块，右边相应顺序的panel的display为block，两者的顺序应该的相对应的--start
+            $(".panel-content > div").each(function () {
+                this.style.display = "none";
+            });
+            $(".panel-content > div").eq(temp).css({
+                display: "block",
+            });
+            //steps中点击相应的块，右边相应顺序的panel的display为block，两者的顺序应该的相对应的--end
+            $(".steps div").eq(temp).css({
+                borderRight: "1px solid rgb(255, 255, 255)",
+                borderTop: "2px solid rgb(6, 243, 13)",
+                borderLeft: "2px solid rgb(6, 243, 13)",
+                borderBottom: "2px solid rgb(6, 243, 13)",
+            });
+            if (temp == 0) {
+                return;
+            }
+            temp -= 1;
+            do {
+                $(".steps div").eq(temp).css({
+                    borderRight: "2px solid rgb(6, 243, 13)",
+                    borderTop: "1px solid rgb(6, 243, 13)",
+                    borderLeft: "2px solid rgb(6, 243, 13)",
+                    borderBottom: "1px solid rgb(6, 243, 13)",
+                });
+                temp -= 1;
+            } while (temp >= 0);
+        }
 
+    });
+
+    $("#preStep").bind("click", function () {
+        var currentPanel;
+        var totalPanel = $(".panel-content > div").length;
+        $(".panel-content > div").each(function (index) {
+            if (this.style.display == "block") {
+                currentPanel = index;
+                this.style.display = "none";
+            }
+        });
+        if (currentPanel == 0) {
+            $(".panel-content > div").each(function (index) {
+                this.style.display = "block";
+                return false;
+            });
+            alert("最开始了");
+            //do {
+            //    $(".steps div").eq(temp).css({
+            //        borderRight: "2px solid rgb(6, 243, 13)",
+            //        borderTop: "1px solid rgb(6, 243, 13)",
+            //        borderLeft: "2px solid rgb(6, 243, 13)",
+            //        borderBottom: "1px solid rgb(6, 243, 13)",
+            //    });
+            //    temp -= 1;
+            //} while (temp >=0);
+            //下一步需要进行自定义logo了
+        } else {
+            $(".panel-content > div").eq(currentPanel - 1).css({
+                display: "block",
+            });
+            $(".steps div").eq(currentPanel).css({
+                borderRight: "1px solid rgb(215, 215, 215)",
+                borderTop: "1px solid rgb(215, 215, 215)",
+                borderLeft: "1px solid rgb(215, 215, 215)",
+                borderBottom: "1px solid rgb(215, 215, 215)",
+            });
+            $(".steps div").eq(currentPanel - 1).css({
+                borderRight: "1px solid rgb(255, 255, 255)",
+                borderTop: "2px solid rgb(6, 243, 13)",
+                borderLeft: "2px solid rgb(6, 243, 13)",
+                borderBottom: "2px solid rgb(6, 243, 13)",
+            });
+        }
+    });
+    $("#nextStep").bind("click", function () {
+        var currentPanel;
+        var totalPanel = $(".panel-content > div").length;
+        $(".panel-content > div").each(function (index) {
+            if (this.style.display == "block") {
+                currentPanel = index;
+                this.style.display = "none";
+            }
+        });
+        if (currentPanel == totalPanel - 1) {
+            alert("最后了");
+            //do {
+            //    $(".steps div").eq(temp).css({
+            //        borderRight: "2px solid rgb(6, 243, 13)",
+            //        borderTop: "1px solid rgb(6, 243, 13)",
+            //        borderLeft: "2px solid rgb(6, 243, 13)",
+            //        borderBottom: "1px solid rgb(6, 243, 13)",
+            //    });
+            //    temp -= 1;
+            //} while (temp >=0);
+            //下一步需要进行自定义logo了
+        } else {
+            $(".panel-content > div").eq(currentPanel + 1).css({
+                display: "block",
+            });
+            $(".steps div").eq(currentPanel).css({
+                borderRight: "2px solid rgb(6, 243, 13)",
+                borderTop: "1px solid rgb(6, 243, 13)",
+                borderLeft: "2px solid rgb(6, 243, 13)",
+                borderBottom: "1px solid rgb(6, 243, 13)",
+            });
+            $(".steps div").eq(currentPanel + 1).css({
+                borderRight: "1px solid rgb(255, 255, 255)",
+                borderTop: "2px solid rgb(6, 243, 13)",
+                borderLeft: "2px solid rgb(6, 243, 13)",
+                borderBottom: "2px solid rgb(6, 243, 13)",
+            });
+        }
+    });
 });
